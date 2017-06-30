bcffec6aec4b9388a91002e1a0056bf5fecea7b4
lxc920223 <sj582192258@sina.com>
2017-06-30 15:36:09 +0800

    林喜纯加了一句“分支代码”测试提交

--- src/com/linxc/Test1.java
+++ src/com/linxc/Test1.java
@@ -7,5 +7,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("测试！");
         System.out.println("修改加个测试！");
+        System.out.println("分支代码！");
     }
 }
