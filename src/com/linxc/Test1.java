45b1daad05b8ade8ffac332fb2ba896ec47e4563
lxc920223 <sj582192258@sina.com>
2017-06-30 16:14:00 +0800

    林喜纯加了"主干代码"测试提交

--- src/com/linxc/Test1.java
+++ src/com/linxc/Test1.java
@@ -7,5 +7,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("测试！");
         System.out.println("修改加个测试！");
+        System.out.println("主干代码！");
     }
 }
