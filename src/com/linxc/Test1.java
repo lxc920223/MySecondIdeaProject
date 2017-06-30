7fbd57a5b628dd1438ce8b3d7ceda682e95e5836
lxc920223 <sj582192258@sina.com>
2017-06-30 14:34:14 +0800

    林喜纯Git Commit Directory测试提交

---
+++ src/com/linxc/Test1.java
@@ -0,0 +1,10 @@
+package com.linxc;
+
+/**
+ * Created by Lin.XiChun on 2017/6/30.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("测试！");
+    }
+}
